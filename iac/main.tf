2bcd1c09b0e56c204d3238796ee1018f71f6aaf0
Josh Batley <josh.batley@hey.com>
2022-06-10 15:28:05 +0100

    iac - added s3

--- iac/main.tf
+++ iac/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  s3_force_path_style         = true
+  s3_use_path_style           = true
 
   endpoints {
     dynamodb = "http://localhost:4566"
@@ -14,5 +14,42 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "edgmont-noticeboard"
+  bucket = "edgmont-images"
+}
+
+resource "aws_s3_bucket_acl" "b" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+resource "aws_dynamodb_table" "d" {
+  read_capacity  = 10
+  write_capacity = 50
+  name           = "edgmont-items"
+  hash_key       = "UserId"
+  range_key      = "GeneratedDate"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GeneratedDate"
+    type = "S"
+  }
+  
+  attribute {
+    name = "Tag"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "UserId-Tagged-index"
+    hash_key        = "UserId"
+    range_key       = "Tag"
+    projection_type = "ALL"
+    read_capacity  = 10
+    write_capacity = 50
+  }
 }
